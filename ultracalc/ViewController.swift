c58509e6b50895366828289b75ca577359f66d9c
Cor Pruijs <cor@CorBook-Yosemite.local>
2014-06-29 16:51:53 +0200

    Initial Commit

---
+++ ultracalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ultracalc
+//
+//  Created by Cor Pruijs on 29-06-14.
+//  Copyright (c) 2014 Cor Pruijs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
